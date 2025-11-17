190b01a3968dfe88026e9790e4ff165da1b77b3e
samngogia <ngogiasamc3@gmail.com>
2025-11-17 22:47:23 +0700

    Thêm folder main chứa java, resources, service, webapp

---
+++ banlaptop/src/main/java/com/example/springWEB/BanlaptopApplication.java
@@ -0,0 +1,13 @@
+package javaweb.banlaptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanlaptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanlaptopApplication.class, args);
+	}
+
+}
---
+++ banlaptop/src/main/java/com/example/springWEB/service/CartDetailService.java
@@ -0,0 +1,48 @@
+package com.example.springWEB.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.springWEB.domain.cart.Cart;
+import com.example.springWEB.domain.cart.CartDetail;
+import com.example.springWEB.repository.CartDetailRepository;
+import com.example.springWEB.repository.OrderDetailRepository;
+
+@Service
+public class CartDetailService {
+
+    private CartDetailRepository cartDetailRepository;
+    private OrderDetailRepository orderDetailRepository;
+    // private CartRepository cartRepository;
+
+    public CartDetailService(CartDetailRepository cartDetailRepository, OrderDetailRepository orderDetailRepository) {
+        this.cartDetailRepository = cartDetailRepository;
+        this.orderDetailRepository = orderDetailRepository;
+        // this.cartRepository = cartRepository;
+    }
+
+    public List<CartDetail> findAllCartDetail() {
+        return this.cartDetailRepository.findAll();
+    }
+
+    public List<CartDetail> findCartDetailByCart(Cart cart) {
+        return this.cartDetailRepository.findByCart(cart);
+    }
+
+    public void deleteCartDetailById(long id) {
+        this.cartDetailRepository.deleteById(id);
+    }
+
+    public CartDetail findCartDetailById(long id) {
+        return this.cartDetailRepository.findById(id);
+    }
+
+    public List<CartDetail> findCartDetailFromCart(Cart cart) {
+        return this.cartDetailRepository.findCartDetailByCart(cart);
+    }
+
+    public void deleteCartDetailByCartt(Cart cart) {
+        this.cartDetailRepository.deleteCartDetailByCart(cart);
+    }
+}
